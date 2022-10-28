8c445134c3890435363acaefeeca044384ed817a
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-28 11:10:07 +0200

    Ejercicios 3 y 4

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,9 +1,11 @@
 '''
 Ejercicio 3. Algoritmo que pida números hasta que se introduzca un cero. Debe imprimir la suma y la media de todos los números introducidos
 '''
-num= 0
-vNum= []
+contador= 0
+suma= 0
+num= (int)(input("Dime un número: \n"))
 while (num!=0):
-    num= (int)(input("Dime un número"))
-    vNum.append(num)
-    print(sum.vNum)
+    contador= contador+1
+    suma= suma+num
+    num= (int)(input("Dime un número: \n"))
+print("La suma total es:", suma, "y la media", (suma/contador))
---
+++ Ejercicio4.py
@@ -0,0 +1,19 @@
+'''
+Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de números a introducir). El programa debe informar de cuantos números introducidos 
+son mayores que 0, menores que 0 e iguales a 0
+'''
+num1= 0
+num2=0
+num3=0
+num4= 0
+print("Dime 4 números")
+num1= (int)(input("\n"))
+num2= (int)(input("\n"))
+num3= (int)(input("\n"))
+num4= (int)(input("\n"))
+contador= []
+
+
+
+while(contador<=0):
+    
