59dbb6002a03a29eb7c8c1cb504a8212b5e0eb82
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-31 10:31:30 +0100

    Ejercicio 4

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -2,13 +2,13 @@
 Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de números a introducir). El programa debe informar de cuantos números introducidos 
 son mayores que 0, menores que 0 e iguales a 0
 '''
-num = []
-menor = 0
-mayor = 0
-igual = 0 
-cant = (int)("Cantidad de números que vas a introducir:\n")
+num= []
+menor= 0
+mayor= 0
+igual= 0 
+cant= (int)(input("Cantidad de números que vas a introducir:\n"))
 
-for i in range(1,cant):
+for i in range(1,cant+1):
     numero = (int)(input("Dime un número:\n"))
     num.append(numero)
 
@@ -20,5 +20,5 @@ for i in num:
     elif i>0:
         mayor+=1
 
-print("numeros mayores que 0:",mayor, "iguales que 0:",  igual, "menores de 0", menor)
+print("numeros mayores que 0:",mayor, "iguales que 0:",  igual, "menores de 0:", menor)
     
